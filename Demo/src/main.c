496648f028860b1a5cb89c0a922cfc7c355b06a4
fossa <fossa@fossa-main.yerrkhao-highspeed.whitefossa.org>
2019-01-27 13:52:44 +0300

    - Added special characters part of KOI8-R

--- Demo/src/main.c
+++ Demo/src/main.c
@@ -65,7 +65,9 @@ int main(int argc, char* argv[])
 
 	/* Forms elements */
 	FMGL_RenderString(&fmglContext, &fontSettings, 0, 0, &drawnWidth, "\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f\x90\x91\x92\x93\x94");
-	FMGL_RenderString(&fmglContext, &fontSettings, 0, 12, &drawnWidth, "\x95\x96\x97\x98\x99\x9a\x9b\x9c\x9d\x9e\x9f");
+	FMGL_RenderString(&fmglContext, &fontSettings, 0, 12, &drawnWidth, "\x95\x96\x97\x98\x99\x9a\x9b\x9c\x9d\x9e\x9f\xa0\xa1\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9");
+	FMGL_RenderString(&fmglContext, &fontSettings, 0, 24, &drawnWidth, "\xaa\xab\xac\xad\xae\xaf\xb0\xb1\xb2\xb3\xb4\xb5\xb6\xb7\xb8\xb9\xba\xbb\xbc\xbd\xbe");
+	FMGL_RenderString(&fmglContext, &fontSettings, 0, 36, &drawnWidth, "\xbf");
 
 	FMGL_PushFramebuffer(&fmglContext);
 
